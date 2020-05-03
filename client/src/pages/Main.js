d784d00def843d02ca326e1d1f96cdd839fc4e5d
Andrew Hardemon <aeroatlas@ymail.com>
2020-05-03 02:55:39 -0700

    added items and some variables to the state

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -12,6 +12,21 @@ class Main extends Component{
   state = {
     character: "Bob",
     location: "Bob's House",
+    description: "This character is a man called Bob. He is the best man whose name starts with the letter B",
+    stats: [
+      {name: "Str", hidden:"false", fullName: "Strength", desc: "Carry Weight, Physical Strength, Inimidation, and Melee", start: 10, stat: 10},
+      {name: "Int", hidden:"false", fullName: "Intelligence", desc: "Problem Solving, Research, Wisdom, and Incredulity", start: 12, stat: 12},
+      {name: "Def", hidden:"false", fullName: "Defence", desc: "Health, Hardiness, Endurance, and Sturdiness", start: 9, stat: 9},
+      {name: "Chr", hidden:"false", fullName: "Charisma", desc: "Likability, Persuasion, Bartering, Empathy", start: 11, stat: 11},
+      {name: "Dex", hidden:"false", fullName: "Dexterity", desc: "Aim , Stealth, Lockpicking, Theft", start: 8, stat: 8},
+      {name: "Agl", hidden:"false", fullName: "Agility", desc: "Speed, Evasion, Stamina, Flexibility", start: 13, stat: 13},
+      {name: "Per", hidden:"false", fullName: "Perception", desc: "Eyesight, Intuition, Observation, Insight", start: 14, stat: 14},
+      {name: "Lck", hidden:"true", fullName: "Luck", desc: "Chance, Gambling, Random Events, Chance of Critical Failure/Success", start: 14, stat: 14},
+      {name: "San", hidden:"true", fullName: "Sanity", desc: ""},
+      {name: "Mor", hidden:"true", fullName: "Morality", desc: ""},
+
+      {Str: 10, Int: 12, Def: 9, Chr:  11, Dex: 8, Agl: 13, Luck: 2, San}
+    ],
     items: [
       {name: "shovel", image: Shovel, data:0}
     ]
@@ -47,11 +62,11 @@ class Main extends Component{
               <div className="inventory">
                 <h2 className="itemBag" style={{color: "white"}}>Inventory</h2>
                   {/* Map through Items and add use, inspect, and throw away buttons */}
+                  <div className="currentItems">
                   {items.map(item => (
-                    <div className="currentItems">
-                      <img style={{width:"150px", height:"150px"}} src={item.image} alt={item.name}></img>
-                    </div>
+                    <img style={{width:"150px", height:"150px"}} src={item.image} alt={item.name}></img>
                   ))}
+                  </div>
               </div>
             </Jumbotron>
           </Col>
