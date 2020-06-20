441fa8311537798d5ac78701a21dc830791e885a
Andrew Hardemon <aeroatlas@ymail.com>
2020-06-19 23:42:58 -0700

    added stats and currently adding effects to change and check different stats

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -20,19 +20,19 @@ class Main extends Component{
       {text: "This character is a man called Bob. He is the best man whose name starts with the letter B"}
     ],
     stats: [
-      {name: "Str", hidden:"false", fullName: "Strength", desc: "Carry Weight, Physical Strength, Inimidation, and Melee", start: 10, current: 10},
-      {name: "Int", hidden:"false", fullName: "Intelligence", desc: "Problem Solving, Research, Wisdom, and Incredulity", start: 12, current: 12},
-      {name: "Def", hidden:"false", fullName: "Defence", desc: "Health, Hardiness, Endurance, and Sturdiness", start: 9, current: 9},
-      {name: "Chr", hidden:"false", fullName: "Charisma", desc: "Likability, Persuasion, Bartering, Empathy", start: 11, current: 11},
-      {name: "Dex", hidden:"false", fullName: "Dexterity", desc: "Aim , Stealth, Lockpicking, Theft", start: 8, current: 8},
-      {name: "Agl", hidden:"false", fullName: "Agility", desc: "Speed, Evasion, Stamina, Flexibility", start: 13, current: 13},
-      {name: "Per", hidden:"false", fullName: "Perception", desc: "Eyesight, Intuition, Observation, Insight", start: 14, current: 14},
-      {name: "Lck", hidden:"false", fullName: "Luck", desc: "Chance, Gambling, Random Events, Chance of Critical Failure/Success", start: 14, current: 14},
-      {name: "San", hidden:"true", fullName: "Sanity", desc: "How well you can handle the horrors of this world. Not everything lasts forever though", start: 20, current: 20},
-      {name: "Mor", hidden:"true", fullName: "Morality", desc: "How perceptive people will see you. You can't hide everything", start: 50, current: 50},
-      {name: "Hum", hidden:"true", fullName: "Humanity", desc: "What is the difference between beast and man?", start: 50, current: 50},
-      {name: "Lif", hidden: "false", fullName: "Life", desc: "How close you are to leaving this world", start: 100, current: 50},
-      {Str: 10, Int: 12, Def: 9, Chr:  11, Dex: 8, Agl: 13, Luck: 2, Sanity: 20}
+      {num: 0, name: "Str", hidden:"false", fullName: "Strength", desc: "Carry Weight, Physical Strength, Inimidation, and Melee", start: 10, current: 10},
+      {num: 1, name: "Int", hidden:"false", fullName: "Intelligence", desc: "Problem Solving, Research, Wisdom, and Incredulity", start: 12, current: 12},
+      {num: 2, name: "Def", hidden:"false", fullName: "Defence", desc: "Health, Hardiness, Endurance, and Sturdiness", start: 9, current: 9},
+      {num: 3, name: "Chr", hidden:"false", fullName: "Charisma", desc: "Likability, Persuasion, Bartering, Empathy", start: 11, current: 11},
+      {num: 4, name: "Dex", hidden:"false", fullName: "Dexterity", desc: "Aim , Stealth, Lockpicking, Theft", start: 8, current: 8},
+      {num: 5, name: "Agl", hidden:"false", fullName: "Agility", desc: "Speed, Evasion, Stamina, Flexibility", start: 13, current: 13},
+      {num: 6, name: "Per", hidden:"false", fullName: "Perception", desc: "Eyesight, Intuition, Observation, Insight", start: 14, current: 14},
+      {num: 7, name: "Lck", hidden:"false", fullName: "Luck", desc: "Chance, Gambling, Random Events, Chance of Critical Failure/Success", start: 14, current: 14},
+      {num: 8, name: "San", hidden:"true", fullName: "Sanity", desc: "How well you can handle the horrors of this world. Not everything lasts forever though", start: 20, current: 20},
+      {num: 9, name: "Mor", hidden:"true", fullName: "Morality", desc: "How perceptive people will see you. You can't hide everything", start: 50, current: 50},
+      {num: 10, name: "Hum", hidden:"true", fullName: "Humanity", desc: "What is the difference between beast and man?", start: 50, current: 50},
+      {num: 11, name: "Lif", hidden: "false", fullName: "Life", desc: "How close you are to leaving this world", start: 100, current: 50}
+      // {Str: 10, Int: 12, Def: 9, Chr:  11, Dex: 8, Agl: 13, Luck: 2, Sanity: 20, Lif: 50}
     ],
     story: [
       {
@@ -40,7 +40,7 @@ class Main extends Component{
         text: "You, after a long day of work, come back to find your house completely destroyed. You are shocked by what you see and run quickly into the house to see that there is nothing left.",
         noun: {name: "house", type: "building", speed: "immobile", living: false, size: "biggest", consumable: false},
         choices:[
-          {num: 0, text: "Cry", effect: ((e) => {this.changeStats("San")})},
+          {num: 0, text: "Cry", effect: ((e) => {this.changeStats("San", (this.state.stats[8].current - 1))})},
           {num: 1, text: "Shoot the wall", effect: ((e) => {this.checkStats("Ammo")})},
           {num: 2, text: "Look through the wreckage"},
           {num: 3, text: "Leave"}
@@ -59,8 +59,8 @@ class Main extends Component{
       }
     ],
     items: [
-      {name: "shovel", image: Shovel, data:0},
-      {name: "knife", image: Knife, data:1}
+      {name: "shovel", image: Shovel, amount: 1, data: 0},
+      {name: "knife", image: Knife, amount: 1, data: 1}
     ]
   }
 
@@ -79,19 +79,25 @@ class Main extends Component{
     
     //Set the state and change to next part
     let nextPart = this.state.part + 1;
+    // Check if you're at the end of the story
+
     this.setState({
       part: nextPart
     });
+
   
   }
 
   // Alter the stats after the event
-  changeStats = (stat) => {
+  changeStats = (stat, amount) => {
     console.log(stat)
+    this.setState({
+      [stat]: amount
+    })
   }
 
   // Check the stats to see what happens next
-  checkStats = (stat) => {
+  checkStats = (stat, amount) => {
     console.log(stat)
   }
 
@@ -132,8 +138,15 @@ class Main extends Component{
                   <h2 style={{color: "white"}}>{character}</h2>
                   <Jumbotron3>
                     <p style={{color: "white"}}>{description[0].text}</p>
+                  
+                
+                  {this.state.stats.map(stat=> (
+                    <p style={{color:"white"}}>
+                      {stat.name}: {stat.current}
+                    </p>
+                  ))}
                   </Jumbotron3>
-                </div>
+                  </div>
               </Jumbotron>
               
             </div>
